c511a76fb02fa5c10d8d7dc1c01d61a6570ec372
rebecca burch <rebecca.burch.stu@gmail.com>
2020-10-09 14:20:45 -0500

    parralax

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import NavBar from './NavBar';
 import Header from './Header';
+import Particles from 'react-particles-js';
+import {makeStyles} from '@material-ui/styles';
+
+const useStyles = makeStyles({
+    particlesCanva:{
+        position: "absolute"
+    }
+})
+
 
 const Home = () => {
+
+    const classes = useStyles()
+
     return (
         <div>
            <NavBar></NavBar>
            <Header/>
+            <Particles
+            canvasClassName={classes.particlesCanva}
+            params={{
+                particles: {
+                    number: {
+                        value: 45,
+                        density: {
+                            enable: true,
+                            value_area: 900
+                        }
+                    },
+                    shape: {
+                        type: "circle",
+                    }
+                }
+            }} >
+
+            </Particles>
         </div>
     )
 }
