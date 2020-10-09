123c33289510a26ba178f8f7b85eb9be7ccd0716
rebecca burch <rebecca.burch.stu@gmail.com>
2020-10-09 17:18:21 -0500

    styled timelineitem

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -35,6 +35,45 @@ const useStyles = makeStyles((theme) => ({
       },
     },
   },
+
+  timeLineItem: {
+      padding: "1rem",
+      borderBottom: "2px solid tan",
+      position: "relative",
+      margin: "1rem 3rem 1rem 1rem",
+      clear: "both",
+      "&:after": {
+        content: "''",
+        position: "absolute"  
+      },
+      "&:before": {
+        content: "''",
+        position: "absolute",
+        right: "0.625rem",
+        top: "calc(50% - 5px)",
+        borderStyle: "solid",
+         borerColor: "tomato tomato transparent transparent",
+          borderWidth: "0.625rem",
+           transform: "rotate(45deg)",
+      },
+      [theme.breakpoints.up("md")]: {
+          width: "44%",
+          margin: "1rem",
+          "&:nth-of-type(2n)": {
+            float: "right",
+            margin: "1rem",
+            borderColor: "tan",
+          },
+          "&:nth-of-type(2n):before": {
+            right: "auto",
+            left: "-0.625rem",
+            borderColor: "transparent transparent tomato tomato"
+          },
+
+      }
+
+  },
+
   timeLineYear: {
     textAlign: "center",
     maxWidth: "9.375rem",
@@ -59,6 +98,12 @@ const useStyles = makeStyles((theme) => ({
       },
     },
   },
+heading: {
+    color: "tomato",
+    padding: "3rem 0",
+    textTransform: "uppercase", 
+}
+
 }));
 
 
@@ -68,11 +113,19 @@ const Resume = () => {
     <>
       <NavBar />
       <Box component="header" className={classes.mainContainer}>
-        <Typography variant="h4" align="center">
+        <Typography className={classes.heading} variant="h4" align="center">
           Learning Experience
         </Typography>
         <Box component="div" className={classes.timeline}>
-          <Typography className={classes.timeLineYear}  variant="h2">2020</Typography>
+          <Typography className={`${classes.timeLineYear} ${classes.timeLineItem}`} variant="h2">2020</Typography>
+          <Box component="div" >
+<Typography variant="h5" align="center">
+web design
+</Typography>
+<Typography variant="body1" align="center">
+company name where worked
+</Typography>
+          </Box>
         </Box>
       </Box>
     </>
