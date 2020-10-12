ad0b014e392c1ee82cca22ce2a07708bd7c1937a
rebecca burch <rebecca.burch.stu@gmail.com>
2020-10-12 14:56:55 -0500

    Updates

---
+++ precache-manifest.0e0d12a49f210f0654a0a4b8b17b7a5f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "116a38846527a4d002afff22a3094624",
+    "url": "/portfolio-app/index.html"
+  },
+  {
+    "revision": "6aec288323df0b8885ae",
+    "url": "/portfolio-app/static/css/main.eeaa3178.chunk.css"
+  },
+  {
+    "revision": "ae73fb6f5d211d6602e4",
+    "url": "/portfolio-app/static/js/2.82d837e3.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/portfolio-app/static/js/2.82d837e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aec288323df0b8885ae",
+    "url": "/portfolio-app/static/js/main.02ff5b96.chunk.js"
+  },
+  {
+    "revision": "9273fb07e8f782d2211c",
+    "url": "/portfolio-app/static/js/runtime-main.9d41a791.js"
+  },
+  {
+    "revision": "7c8e5f50725885d51b72b45164274e41",
+    "url": "/portfolio-app/static/media/avatar.7c8e5f50.png"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/portfolio-app/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "54fd423489a1937caf8950d12c8a8526",
+    "url": "/portfolio-app/static/media/html-css-javascript-lg.54fd4234.jpg"
+  },
+  {
+    "revision": "730958a228d26dee011c5aa0aa3c3db9",
+    "url": "/portfolio-app/static/media/javascript-fullstack.730958a2.jpg"
+  },
+  {
+    "revision": "90616a5dc9acdcce7158addc89620052",
+    "url": "/portfolio-app/static/media/mern-stack.90616a5d.jpg"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/portfolio-app/static/media/react-redux.a8b012a0.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/thinklikeadesigner/portfolio-app-2/precache-manifest.95046d57832a8c26e8c11b002afa835c.js"
+  "/portfolio-app/precache-manifest.0e0d12a49f210f0654a0a4b8b17b7a5f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/thinklikeadesigner/portfolio-app-2/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
